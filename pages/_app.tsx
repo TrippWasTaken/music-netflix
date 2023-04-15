bc42e2a12c4aeb252ed500d42feebaf4c25de1a3
Konrad Knecht <C16395636@mytudublin.ie>
2023-04-15 19:39:39 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const lightTheme = createTheme({
+    type: 'light'
+  });
+
+  const darkTheme = createTheme({
+    type: 'dark'
+  });
+
+  return (
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{ light: lightTheme.className, dark: darkTheme.className }}
+    >
+      <NextUIProvider>
+        <style global jsx>{`
+          html,
+          body,
+          body > div:first-child,
+          div#__next,
+          div#__next > div {
+            height: 100%;
+            width: 100%;
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </ThemeProvider>
+  );
 }
---
+++ pages/auth.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import { Card, Text, Container, Row, Col, Input, Spacer, Button, Link } from '@nextui-org/react';
+import { GoogleCircleFilled, GithubFilled } from '@ant-design/icons';
+
+const Auth = () => {
+  const [email, setEmail] = useState<String | null>(null);
+  const [username, setUsername] = useState<String | null>(null);
+  const [password, setPassword] = useState<String | null>(null);
+  const [signup, setSignup] = useState<Boolean>(false);
+  const { Body, Header, Footer } = Card;
+  const { Password } = Input;
+
+  const toggleSignup = () => {
+    setSignup((prev: Boolean) => !prev);
+  };
+
+  const confirmAction = () => {};
+
+  return (
+    <Container fluid css={{ padding: '$2xl $xl ' }}>
+      <Row justify="center">
+        <Card
+          css={{
+            width: '100%',
+            '@sm': {
+              width: '$8xl'
+            }
+          }}
+        >
+          <Header>
+            <Row justify="center">
+              <Text h1 size={30}>
+                {signup ? 'Sign In' : 'Sign Up'}
+              </Text>
+            </Row>
+          </Header>
+          <Body>
+            {signup && (
+              <>
+                <Input bordered fullWidth labelPlaceholder="Username" onBlur={e => setUsername(e.target.value)} />
+                <Spacer y={2} />
+              </>
+            )}
+            <Input bordered fullWidth labelPlaceholder="Email" onBlur={e => setEmail(e.target.value)} />
+            <Spacer y={2} />
+            <Password bordered fullWidth labelPlaceholder="Password" onBlur={e => setPassword(e.target.value)} />
+            <Spacer y={1} />
+            {signup ? (
+              <Text>
+                Already registered?{' '}
+                <Text
+                  b
+                  color="secondary"
+                  css={{
+                    textDecoration: 'underline',
+                    '@hover': {
+                      cursor: 'pointer'
+                    }
+                  }}
+                  onClick={() => toggleSignup()}
+                >
+                  sign in!
+                </Text>
+              </Text>
+            ) : (
+              <Text>
+                Need to{' '}
+                <Text
+                  b
+                  color="secondary"
+                  css={{
+                    textDecoration: 'underline',
+                    '@hover': {
+                      cursor: 'pointer'
+                    }
+                  }}
+                  onClick={() => toggleSignup()}
+                >
+                  sign up
+                </Text>{' '}
+                first?
+              </Text>
+            )}
+          </Body>
+          <Footer css={{ display: 'flex', flexDirection: 'column' }}>
+            <Row>
+              <Col>
+                <Button onPress={() => null} css={{ width: '100%' }}>
+                  {signup ? 'Sign up' : 'Login'}
+                </Button>
+              </Col>
+            </Row>
+            <Spacer y={1} />
+            <Text size={'$sm'} css={{ opacity: '50%' }}>
+              Wanna use something else?
+            </Text>
+            <Row css={{ paddingTop: '$md', borderTop: '1px solid $accents1' }}>
+              <Col>
+                <Button
+                  onPress={() => null}
+                  css={{ width: '100%' }}
+                  icon={<GoogleCircleFilled />}
+                  bordered
+                  color="secondary"
+                  iconRight
+                >
+                  Use google
+                </Button>
+              </Col>
+            </Row>
+            <Row css={{ paddingTop: '$md', borderTop: '1px solid $accents1' }}>
+              <Col>
+                <Button
+                  onPress={() => null}
+                  css={{ width: '100%' }}
+                  icon={<GithubFilled />}
+                  bordered
+                  color="secondary"
+                  iconRight
+                >
+                  Use Github
+                </Button>
+              </Col>
+            </Row>
+          </Footer>
+        </Card>
+      </Row>
+    </Container>
+  );
+};
+
+export default Auth;
